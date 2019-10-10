8058a35c87bdb7fd1bc4ab38bdb76fccca25df02
David Decker <david.decker@apptiva.ch>
2019-10-10 12:10:41 +0200

    no-html

--- todo.js
+++ todo.js
@@ -1,39 +1,78 @@
+let card = document.createElement("div")
+card.setAttribute("class","card")
+
+let listTitle = document.createElement("div")
+listTitle.setAttribute("class", "list-title")
+
+let clear = document.createElement("i")
+clear.setAttribute("class", "fas fa-undo-alt")
+
+let date = document.createElement("h1")
+date.setAttribute("class", "date")
+
+let itemList = document.createElement("div")
+itemList.setAttribute("id", "item-list")
+
+let inputContainer = document.createElement("div")
+inputContainer.setAttribute("class", "input")
+
+let plus = document.createElement("i")
+plus.setAttribute("class", "fas fa-plus-circle")
+
+let input = document.createElement("input")
+input.setAttribute("type", "text")
+input.setAttribute("name", "input")
+
+function addChildren(parent,children){
+	for (let i = 0; i < children.length; i++){
+		parent.appendChild(children[i])
+	}
+}
+
+addChildren(document.body,[card])
+addChildren(card,[listTitle,itemList,inputContainer])
+addChildren(listTitle, [date, clear])
+addChildren(inputContainer, [plus, input])
+
+
+
 const options = {weekday: 'long', day: 'numeric', month: 'short', year: 'numeric'};
+const today = new Date();
 
-const date = new Date();
-
-document.getElementById('date').innerHTML = date.toLocaleDateString('en-GB', options);
-
-function getElementById(asdf){
-	return "element with id "+asdf
-}
-
-console.log(getElementById("a cool id"))
-
-let input = document.getElementById("input");
-
-let card = document.getElementById("card");
-
-function doSomething(){
-	console.log("i'm doing something")
-	return "a cool value"
-}
+date.innerHTML = today.toLocaleDateString('en-GB', options);
 
 
-let resultOfDosomting = doSomething()
-console.log("result of doSomething",resultOfDosomting)
+
 
 function insertItem(value){
-	itemList.insertAdjacentHTML('beforeend', '<ul class="list"><li class="list-item"><input type="checkbox" name="checkbox" id="checkbox">'+value+'<i class="far fa-trash-alt" id="trash"></i></li></ul>');
-	let trash = document.getElementById("trash");
-	trash.removeAttribute('id');
+	let list = document.createElement("ul")
+	list.setAttribute("class","list")
+
+	let listItem = document.createElement("li")
+	listItem.setAttribute("class","list-item")
+
+	let text = document.createElement("span")
+	text.setAttribute("class","list-item")
+	text.innerText = input.value
+	
+	let checkbox = document.createElement("input")
+	checkbox.setAttribute("type","checkbox")
+	checkbox.setAttribute("name","checkbox")
+
+	let trash = document.createElement("i")
+	trash.setAttribute("class","far fa-trash-alt")
+
+	addChildren(itemList,[list])
+	addChildren(list,[listItem])
+	addChildren(listItem,[checkbox,text,trash])
+
 	trash.addEventListener('click', function(e) {
 		console.log(e);
 		trash.parentNode.parentNode.removeChild(trash.parentNode);
 	})
 }
 
-document.getElementById("plus").addEventListener('click', function(e) {
+plus.addEventListener('click', function(e) {
 	console.log(e)
 	if (input.value.length >0) {
 		insertItem(input.value);
@@ -42,11 +81,10 @@ document.getElementById("plus").addEventListener('click', function(e) {
 	}
 });
 
-let itemList = document.getElementById("item-list");
 
 let numberOfItems = itemList.childNodes;
 
-document.getElementById("clear").addEventListener('click', function(e) {
+clear.addEventListener('click', function(e) {
 	console.log(e);
 	while (numberOfItems.length > 0) {
 		itemList.removeChild(itemList.firstChild);
